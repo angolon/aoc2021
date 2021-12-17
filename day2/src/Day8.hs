3c26c0927cf91d608901f36d3f58aef072efee7a
Angus Gerry <angus.gerry@quantium.com.au>
2021-12-17 11:38:49 +1100

    Day 8 - part 2: complete

--- day2/src/Day8.hs
+++ day2/src/Day8.hs
@@ -5,6 +5,11 @@
 module Day8 where
 
 import Data.Bits
+import Data.Foldable
+import qualified Data.List.NonEmpty as NE
+import Data.Map (Map, (!))
+import qualified Data.Map as Map
+import Data.Monoid (Sum (..), getSum)
 import Data.Word
 import Lib (MyParser, parseStdin)
 import qualified SevenSegment as SS
@@ -22,7 +27,46 @@ parseDisplay = Display <$> parseNPatterns 10 <* space <* char '|' <* space <*> p
 parseDisplays :: MyParser [Display]
 parseDisplays = many1 parseDisplay <* eof
 
+solve :: Display -> Int
+solve (Display patterns outputs) =
+  let numBitsMap = SS.groupByNumBits patterns
+      one = NE.head $ numBitsMap ! (popCount SS.one)
+      four = NE.head $ numBitsMap ! (popCount SS.four)
+      seven = NE.head $ numBitsMap ! (popCount SS.seven)
+      eight = NE.head $ numBitsMap ! (popCount SS.eight)
+      ne235 = numBitsMap ! (popCount SS.two)
+      ne069 = numBitsMap ! (popCount SS.zero)
+      (Just three) = find (\x -> all (\y -> x == y || ((SS.numUniqueBits x y) == 1)) ne235) ne235
+      (Just two) = find (\x -> [1, 1, 2, 1, 2] == (SS.numUniqueBits <$> [one, three, four, seven, eight] <*> [x])) ne235
+      (Just five) = find (\x -> x /= three && x /= two) ne235
+      matchRemaining counts =
+        find
+          ( \x ->
+              counts == (SS.numUniqueBits <$> [one, two, three, four, five, seven, eight] <*> [x])
+          )
+          ne069
+      (Just zero) = matchRemaining [0, 1, 1, 1, 1, 0, 1]
+      (Just six) = matchRemaining [1, 1, 1, 1, 0, 1, 1]
+      (Just nine) = matchRemaining [0, 1, 0, 0, 0, 0, 1]
+      descrambler =
+        Map.fromList
+          [ (zero, 0),
+            (one, 1),
+            (two, 2),
+            (three, 3),
+            (four, 4),
+            (five, 5),
+            (six, 6),
+            (seven, 7),
+            (eight, 8),
+            (nine, 9)
+          ]
+   in foldl' (\value digit -> (value * 10) + (descrambler ! digit)) 0 outputs
+
+solveMany :: [Display] -> Int
+solveMany = getSum . foldMap (Sum . solve)
+
 descrambleDisplays :: IO ()
 descrambleDisplays = do
   parsed <- parseStdin parseDisplays
-  print parsed
+  print $ fmap solveMany parsed
--- day2/src/SevenSegment.hs
+++ day2/src/SevenSegment.hs
@@ -2,6 +2,10 @@ module SevenSegment where
 
 import Data.Bits
 import Data.Foldable (foldl')
+import Data.List.NonEmpty (NonEmpty)
+import qualified Data.List.NonEmpty as NE
+import Data.Map (Map)
+import qualified Data.Map as Map
 import Data.Word
 import Lib (MyParser)
 import Text.Parsec
@@ -50,6 +54,9 @@ eight = a .|. b .|. c .|. d .|. e .|. f .|. g
 
 nine = a .|. b .|. c .|. d .|. f .|. g
 
+digits :: [SevenSegment]
+digits = [zero, one, two, three, four, five, six, seven, eight, nine]
+
 lookupBitChar :: Char -> SevenSegment
 lookupBitChar ch = case ch of
   'a' -> a
@@ -69,3 +76,34 @@ parseBit = lookupBitChar <$> parseBitChar
 
 parseSevenSegment :: MyParser SevenSegment
 parseSevenSegment = foldl' (.|.) 0 <$> many1 parseBit
+
+numCommonBits :: SevenSegment -> SevenSegment -> Int
+numCommonBits a b = popCount $ a .&. b
+
+uniqueBits :: SevenSegment -> SevenSegment -> SevenSegment
+uniqueBits a b = a .&. (a `xor` b)
+
+numUniqueBits :: SevenSegment -> SevenSegment -> Int
+numUniqueBits a b = popCount $ uniqueBits a b
+
+uniqueBitsAll :: SevenSegment -> [SevenSegment] -> SevenSegment
+uniqueBitsAll a = foldl' uniqueBits a
+
+keyBy :: (a -> k) -> a -> (k, a)
+keyBy f a = (f a, a)
+
+groupByNumBits :: [SevenSegment] -> Map Int (NonEmpty SevenSegment)
+groupByNumBits sss = Map.fromList . fmap (keyBy $ popCount . NE.head) . NE.groupAllWith popCount $ sss
+
+numBitsToDigits :: Map Int (NonEmpty SevenSegment)
+numBitsToDigits = groupByNumBits digits
+
+-- Maps un-scrambled digits
+-- numUniqueBits :: Map SevenSegment Int
+-- numUniqueBits =
+--   let kds = do
+--         k <- digits
+--         let otherDigits = filter (/= k) digits
+--         let u = uniqueBitsAll k otherDigits
+--         return (k, popCount u)
+--    in Map.fromList kds
