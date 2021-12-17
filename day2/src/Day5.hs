55976abe47ae2a725fd5a025ca7fb881f8012af7
Angus Gerry <angus.gerry@quantium.com.au>
2021-12-17 11:38:49 +1100

    I am ashamed

--- day2/src/Day5.hs
+++ day2/src/Day5.hs
@@ -7,7 +7,11 @@ module Day5 where
 import Control.Arrow
 import Control.Lens
 import Control.Monad
+import Data.Either (isRight)
+import Data.Foldable (traverse_)
+import qualified Data.Map.Monoidal as MMap
 import qualified Data.Maybe as Maybe
+import Data.Monoid (Sum (..))
 import Data.Ratio
 import qualified Data.Set as Set
 import Lib (MyParser, parseInt, parseStdin)
@@ -39,7 +43,7 @@ inBound (Bound lower upper) p = lower <= p && p <= upper
 -- Duplicates "awesome" stuff done with arrows - could maybe make bounds
 -- representable as functions.
 -- WARNING  - is this fucked?
-boundOverlap :: Bound -> Bound -> Bound
+boundOverlap :: Bound -> Bound -> Maybe Bound
 boundOverlap
   (Bound (x1, y1) (x2, y2))
   (Bound (x3, y3) (x4, y4)) =
@@ -47,7 +51,9 @@ boundOverlap
         highX = min x2 x4
         lowY = max y1 y3
         highY = min y2 y4
-     in if lowX > highX || lowY > highY then error "god damn it" else Bound (lowX, lowY) (highX, highY)
+     in if lowX > highX || lowY > highY
+          then Nothing
+          else Just $ Bound (lowX, lowY) (highX, highY)
 
 xToY :: Dimension -> LineEquation -> Maybe Dimension
 xToY _ (Horizontal y) = Just y
@@ -81,6 +87,46 @@ intersection g@(Gradient _ _) h = intersection h g
 
 data LineSegment = LineSegment {_line :: LineEquation, _bound :: Bound} deriving (Eq, Show)
 
+inBothArr b1 b2 = arr inBound b1 &&& arr inBound b2
+
+inBoth b1 b2 = allOf both id . inBothArr b1 b2
+
+sanityCheckPoint (Vertical x) (Horizontal y) (px, py) = x == px && y == py
+sanityCheckPoint (Gradient m b) (Gradient n c) (px, py) =
+  py == (m * px) + b
+    && py == (n * px) + c
+sanityCheckPoint (Gradient m b) (Horizontal y) (px, py) =
+  py == y && ((m * px) + b) == py
+sanityCheckPoint (Gradient m b) (Vertical x) (px, py) =
+  px == x && ((m * px) + b) == py
+sanityCheckPoint (Vertical x1) (Vertical x2) (px, _) = x1 == x2 && x2 == px
+sanityCheckPoint (Horizontal y1) (Horizontal y2) (_, py) = y1 == y2 && y2 == py
+sanityCheckPoint l1 l2 p = sanityCheckPoint l2 l1 p
+
+sanityCheck (LineSegment l1 b1) (LineSegment l2 b2) (Just p) =
+  if (sanityCheckPoint l1 l2 p) && (inBoth b1 b2 p) then Right () else Left "damn it"
+
+sanityCheckOverlap _ _ Nothing = Right ()
+sanityCheckOverlap ls1@(LineSegment l1 b1) ls2@(LineSegment l2 b2) (Just ls3@(LineSegment l3 b3)) =
+  let equationEquality = l1 == l2 && l2 == l3
+      boundIntersection = (Just b3) == (boundOverlap b1 b2)
+      points = segmentPoints ls3
+      pointsInBounds = all (\p -> inBound b1 p && inBound b2 p && inBound b3 p) points
+   in if ls1 == ls2
+        then Left "the same things are in here"
+        else
+          if equationEquality && boundIntersection && pointsInBounds
+            then traverse_ (sanityCheck ls1 ls2) (map Just points)
+            else Left "overlap is fucked"
+
+boundCheck (((LineSegment _ b1), (LineSegment _ b2)), (Just ls@(LineSegment _ b3))) =
+  let ps = segmentPoints ls
+      arrowed = fmap (inBoth b1 b2) ps
+      blarghed = fmap (inBound b3) ps
+   in arrowed == blarghed
+
+-- sanityCheck (LineSegment
+
 -- I have made this function as over-complicated as I possibly could,
 -- because... I felt like it? ⁻\_(ツ)_/⁻
 segmentIntersection :: LineSegment -> LineSegment -> Maybe Point
@@ -95,7 +141,7 @@ segmentOverlap :: LineSegment -> LineSegment -> Maybe LineSegment
 segmentOverlap
   (LineSegment l1 b1)
   (LineSegment l2 b2)
-    | l1 == l2 = Just (LineSegment l1 (boundOverlap b1 b2))
+    | l1 == l2 = LineSegment l1 <$> boundOverlap b1 b2
     | otherwise = Nothing
 
 segmentPoints :: LineSegment -> [Point]
@@ -120,7 +166,8 @@ segmentsDanger ls1 ls2
   | ls1 /= ls2 =
     let intersection = Maybe.maybeToList $ segmentIntersection ls1 ls2
         overlap = segmentOverlap ls1 ls2
-        overlapPoints = segmentPoints =<< (Maybe.maybeToList overlap)
+        overlapSanity = sanityCheckOverlap ls1 ls2 overlap
+        overlapPoints = if isRight overlapSanity then segmentPoints =<< (Maybe.maybeToList overlap) else error "fuuuuck"
      in overlapPoints ++ intersection
   | otherwise = []
 
@@ -147,6 +194,14 @@ countDistinct :: (Ord a) => [a] -> Int
 countDistinct = Set.size . Set.fromList
 
 countDanger = countDistinct . findDanger
+
+fuckThis :: [LineSegment] -> Int
+fuckThis segments =
+  let allPoints = segmentPoints =<< segments
+      pointCounts :: MMap.MonoidalMap Point (Sum Int)
+      pointCounts = foldMap (\p -> MMap.singleton p (Sum 1)) allPoints
+      interestingPoints = MMap.keys . MMap.filter (> 1) $ pointCounts
+   in length interestingPoints
 
 parseDimension :: MyParser Dimension
 parseDimension = (% 1) <$> parseInt
@@ -177,4 +232,4 @@ parseLineSegments = many1 (parseLineSegment <* endOfLine) <* eof
 avoidDanger :: IO ()
 avoidDanger = do
   parsed <- parseStdin (parseLineSegments)
-  print $ fmap (countDanger) parsed
+  print $ fmap (fuckThis) parsed
