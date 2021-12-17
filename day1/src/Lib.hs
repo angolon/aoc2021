d8fa471a823b3b477c82372bb10a24ac392fdea3
Angus Gerry <angus.gerry@quantium.com.au>
2021-12-17 11:38:48 +1100

    Day 1, part 1.

--- day1/src/Lib.hs
+++ day1/src/Lib.hs
@@ -1,6 +1,26 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE TypeApplications #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib
+  ( increases,
+  )
+where
+
+readInput :: IO String
+readInput = getContents
+
+parseInput :: (Read a) => String -> [a]
+parseInput = (fmap read) . lines
+
+thing :: (Ord a) => [a] -> [Bool]
+thing xs = (zipWith (>)) (tail xs) xs
+
+countIncreases = length . (filter id)
+
+-- parseInput = readList . linesk
+
+increases :: IO ()
+increases = do
+  input <- getContents
+  let xs = (parseInput @Int) input
+  let c = countIncreases . thing $ xs
+  print c
