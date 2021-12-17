4d50f547825ed77a8acdb802aed712e165d2f720
Angus Gerry <angus.gerry@quantium.com.au>
2021-12-17 11:38:48 +1100

    Over generalised bullshit.

--- day1/src/Lib.hs
+++ day1/src/Lib.hs
@@ -2,13 +2,14 @@
 
 module Lib
   ( increases,
+    slidingCo,
   )
 where
 
 import Control.Applicative
-
-readInput :: IO String
-readInput = getContents
+import Control.Comonad
+import qualified Data.List.NonEmpty as NE
+import Data.Monoid (Sum)
 
 parseInput :: (Read a) => String -> [a]
 parseInput = (fmap read) . lines
@@ -18,11 +19,32 @@ sliding2 f as =
   let zippedBs = f <$> (ZipList as) <*> (ZipList (drop 1 as))
    in getZipList zippedBs
 
+sliding3 :: (a -> a -> a -> b) -> [a] -> [b]
+sliding3 f as =
+  let zippedBs = f <$> (ZipList as) <*> (ZipList (drop 1 as)) <*> (ZipList (drop 2 as))
+   in getZipList zippedBs
+
+slidingCo :: Int -> NE.NonEmpty a -> (ZipList [a])
+slidingCo n as =
+  let nTails = (NE.take n) . duplicate $ as
+   in traverse (ZipList . NE.toList) nTails
+
+-- sliding2Co :: (a -> a -> b) -> [a] -> [b]
+-- sliding2Co _ [] = []
+-- sliding2Co f as =
+--   let zippedBs = f <$> (ZipList as) <*> (ZipList (drop 1 as))
+--    in getZipList zippedBs
+
+countIncreases :: (Ord a) => [a] -> Int
 countIncreases = length . (filter id) . (sliding2 (<))
+
+sumWindows3 :: (Semigroup a) => [a] -> [a]
+sumWindows3 as = sliding3 (\a1 a2 a3 -> a1 <> a2 <> a3) as
 
 increases :: IO ()
 increases = do
   input <- getContents
   let xs = (parseInput @Int) input
+  -- let c = countIncreases . sumWindows3 $ (fmap Sum xs)
   let c = countIncreases xs
   print c
