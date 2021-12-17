81f8024f93e08315a71c03ca4b84e1f4e5072a97
Angus Gerry <angus.gerry@quantium.com.au>
2021-12-17 11:38:48 +1100

    A thing that won't work because apparently vent lines can have the same underlying equation (ノ°□°)ノ⌒┻━┻

--- day2/src/Day5.hs
+++ day2/src/Day5.hs
@@ -3,7 +3,9 @@
 
 module Day5 where
 
+import Control.Arrow
 import Control.Lens
+import Control.Monad
 import Lib (MyParser, parseInt, parseStdin)
 import Text.Parsec
 import Text.Parsec.Char
@@ -11,6 +13,38 @@ import Text.Parsec.Char
 type Point = (Int, Int)
 
 data VentLine = VentLine {_a :: Point, _b :: Point} deriving (Eq, Show)
+
+data LineEquation
+  = Horizontal {_y :: Int}
+  | Vertical {_x :: Int}
+  deriving (Eq, Show)
+
+data Bound = Bound {_lower :: Point, _upper :: Point} deriving (Eq, Show)
+
+-- Constructor that enforces ordering of bounds.
+bound :: Point -> Point -> Bound
+bound (x1, y1) (x2, y2) =
+  Bound (min x1 x2, min y1 y2) (max x1 x2, max y1 y2)
+
+inBound :: Bound -> Point -> Bool
+inBound (Bound lower upper) p = lower <= p && p <= upper
+
+intersection :: LineEquation -> LineEquation -> Maybe Point
+intersection h@(Horizontal _) v@(Vertical _) = intersection v h
+intersection (Vertical x) (Horizontal y) = Just (x, y)
+intersection _ _ = Nothing
+
+data LineSegment = LineSegment {_line :: LineEquation, _bound :: Bound} deriving (Eq, Show)
+
+-- I have made this function as over-complicated as I possibly could,
+-- because... I felt like it? ⁻\_(ツ)_/⁻
+segmentIntersection :: LineSegment -> LineSegment -> Maybe Point
+segmentIntersection
+  (LineSegment l1 b1)
+  (LineSegment l2 b2) =
+    let inBothArr = arr inBound b1 &&& arr inBound b2
+        inBoth = allOf both id . inBothArr
+     in mfilter inBoth (intersection l1 l2)
 
 parsePoint :: MyParser Point
 parsePoint = (,) <$> parseInt <* spaces <* char ',' <*> parseInt
