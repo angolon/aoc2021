6acf10dad8d5c9d748f8efae9906d087a43b8012
Angus Gerry <angus.gerry@quantium.com.au>
2021-12-17 11:38:48 +1100

    Tidying

--- day1/src/Lib.hs
+++ day1/src/Lib.hs
@@ -5,22 +5,24 @@ module Lib
   )
 where
 
+import Control.Applicative
+
 readInput :: IO String
 readInput = getContents
 
 parseInput :: (Read a) => String -> [a]
 parseInput = (fmap read) . lines
 
-thing :: (Ord a) => [a] -> [Bool]
-thing xs = (zipWith (>)) (tail xs) xs
+sliding2 :: (a -> a -> b) -> [a] -> [b]
+sliding2 f as =
+  let zippedBs = f <$> (ZipList as) <*> (ZipList (drop 1 as))
+   in getZipList zippedBs
 
-countIncreases = length . (filter id)
-
--- parseInput = readList . linesk
+countIncreases = length . (filter id) . (sliding2 (<))
 
 increases :: IO ()
 increases = do
   input <- getContents
   let xs = (parseInput @Int) input
-  let c = countIncreases . thing $ xs
+  let c = countIncreases xs
   print c
