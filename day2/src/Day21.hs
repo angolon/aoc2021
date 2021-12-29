7033a7ea479a75073aab7d60e20f595f0429ab06
Angus Gerry <angus.gerry@quantium.com.au>
2021-12-29 22:34:00 +1100

    An endomorphism to fracture the multiverse for a single player.

--- day2/src/Day21.hs
+++ day2/src/Day21.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -39,16 +40,17 @@ import Text.Parsec
 import Text.Parsec.Char
 import Text.Show.Functions
 
-newtype Board = Board {_getBoard :: [Int]}
+newtype Score = Score {_getScore :: Int} deriving (Show, Eq, Ord, Num)
 
--- Equality for boards which assumes that if the head is equal, then the whole thing is equal.
--- Won't work for boards of potentially different length.
-instance Eq Board where
-  (==) (Board (a : _)) (Board (b : _)) = a == b
+newtype Position = Position {_getPos :: Int} deriving (Show, Eq, Ord, Num, Enum)
 
--- Assumes that the length is 10 :-P
-instance Show Board where
-  show (Board b) = show $ take 10 b
+makeLenses ''Score
+makeLenses ''Position
+
+-- toScore :: Position -> Score
+toScore p = Score $ p ^. getPos
+
+newtype Board = Board {_getBoard :: [Position]}
 
 makeLenses ''Board
 
@@ -56,43 +58,13 @@ newtype Die = Die {_getDie :: [Int]} deriving (Eq, Show)
 
 makeLenses ''Die
 
-data Player = Player
-  { _name :: Int,
-    _position :: Int,
-    _score :: Int,
-    _continuingBoard :: Board
-  }
-  deriving (Eq, Show)
-
-makeLenses ''Player
-
-data Game = Game
-  { _nRolls :: Int,
-    _players :: (Player, Player),
-    _die :: Die
-  }
-  deriving (Eq, Show)
-
-makeLenses ''Game
-
 board :: Board
-board = Board $ List.cycle [1 .. 10]
+board = Board $ List.cycle [(Position 1) .. (Position 10)]
 
-player :: Int -> Int -> Player
-player name startingPosition =
-  Player name startingPosition 0 (board & getBoard %~ drop startingPosition)
+finiteBoard :: Board
+finiteBoard = board & getBoard %~ take 10
 
-initialiseGame :: Player -> Player -> Game
-initialiseGame player1 player2 =
-  Game 0 (player1, player2) (Die [1 .. 3])
-
-move :: Int -> Player -> Player
-move n player =
-  let withNextBoard = player & (continuingBoard . getBoard) %~ drop n
-      (Just nextPosition) = firstOf (continuingBoard . getBoard . folded) withNextBoard
-   in withNextBoard & position .~ nextPosition & score +~ nextPosition
-
-type GameState = S.StateT Game []
+d3 = Die [1 .. 3]
 
 -- tuple of probability and sum of 3 rolls
 quantumDieRoll :: Die -> [(Rational, Int)]
@@ -104,85 +76,34 @@ quantumDieRoll (Die d) =
       groupRoll = NonEmpty.head
    in fmap (groupProbability &&& groupRoll) groupedRolls
 
-nextStepProbabilities :: Die -> Map Int [(Rational, Int)]
-nextStepProbabilities d =
-  let rolls = quantumDieRoll d
+nextStepProbabilities :: Map Position [(Rational, Position)]
+nextStepProbabilities =
+  let rolls = quantumDieRoll d3
    in Map.fromList $ do
-        i <- [1 .. 10]
-        let continuingBoard = drop (i - 1) . _getBoard $ board
+        position <- _getBoard finiteBoard
+        let continuingBoard = drop ((_getPos position) - 1) . _getBoard $ board
         let nextPositions = rolls & (mapped . _2) %~ head . (`drop` continuingBoard)
-        return (i, nextPositions)
+        return (position, nextPositions)
 
-multiverse :: Int -> [(Rational, Int)]
-multiverse startPos =
-  let d = Die [1 .. 3]
-      nextStepProbs = nextStepProbabilities d
-      step :: (Rational, Int, Int) -> [(Rational, Int, Int)]
-      step (p, i, score) =
-        let steps = nextStepProbs ! i
-         in fmap (\(q, j) -> (p * q, j, score + j)) steps
-      -- I think there's something magical about the number 6,
-      -- due to vague maths.
-      blargh =
-        step (1, startPos, 0) >>= step >>= step >>= step >>= step >>= step
-          >>= step
-          >>= step
-   in fmap (\(a, b, c) -> (a, c)) blargh
+type Multiverse = MonoidalMap (Score, Position) (Sum Rational)
 
-moveStream :: Int -> Int -> [Player]
-moveStream name startPos = undefined
+fracture :: Score -> Position -> Sum Rational -> Multiverse
+fracture score position (Sum probability) =
+  let steps = nextStepProbabilities ! position
+      nextScoreProb (p, position) =
+        let nextScore = score + (toScore position)
+            nextProbability = p * probability
+         in MMap.singleton (nextScore, position) (Sum nextProbability)
+   in foldMap nextScoreProb steps
 
-roll :: GameState [Int]
-roll = do
-  game <- S.get
-  let d = game ^. die
-  (probability, rollTotal) <- S.lift $ quantumDieRoll d
-  let game' = game & nRolls +~ 3
-  S.put game'
-  return [rollTotal]
+fractureMultiverse :: Multiverse -> Multiverse
+fractureMultiverse = MMap.foldMapWithKey $ uncurry fracture
 
-turn :: GameState ()
-turn = do
-  rolls <- roll
-  game <- S.get
-  let (player1, player2) = game ^. players
-  let rollTotal = sum rolls
-  let nextPlayer1 = move rollTotal player1
-  let nextGame = game & players .~ (player2, nextPlayer1)
-  S.put nextGame
-
-shouldStop :: GameState Bool
-shouldStop = do
-  game <- S.get
-  return $ anyOf (players . both . score) (>= 21) game
-
-playGame :: GameState ()
-playGame = Loops.untilM_ turn shouldStop
-
-winCounts :: GameState (MonoidalMap Int (Sum Integer))
-winCounts = do
-  game <- S.get
-  let (Just winner) = maximumByOf (players . both) (compare `on` (^. score)) game
-  let n = winner ^. name
-  return $ MMap.singleton n (Sum 1)
-
-playQuantumGames :: Game -> MonoidalMap Int (Sum Integer)
-playQuantumGames initial =
-  let horrific = playGame >> winCounts
-      multiverse = S.evalStateT horrific initial
-   in fold multiverse
-
-finalMunge :: Game -> Int
-finalMunge game =
-  let (Just losingScore) = minimumOf (players . both . score) game
-   in losingScore * (game ^. nRolls)
+initialUniverse :: Position -> Multiverse
+initialUniverse position = MMap.singleton ((Score 0), position) (Sum 1)
 
 playWithDice :: IO ()
 playWithDice =
   let player1Pos = 4 - 1
       player2Pos = 8 - 1
-      player1 = player 1 player1Pos
-      player2 = player 2 player2Pos
-      initial = initialiseGame player1 player2
-      finished = playQuantumGames initial
-   in print finished
+   in putStrLn "lolololol"
