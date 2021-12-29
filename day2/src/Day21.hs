e1891f99030f62d3e41496f94de86cd95e9bb473
Angus Gerry <angus.gerry@quantium.com.au>
2021-12-29 22:34:00 +1100

    Hacking around with probabilities

--- day2/src/Day21.hs
+++ day2/src/Day21.hs
@@ -19,7 +19,7 @@ import Data.Function (on)
 import qualified Data.List as List
 import Data.List.NonEmpty (NonEmpty (..))
 import qualified Data.List.NonEmpty as NonEmpty
-import Data.Map (Map)
+import Data.Map (Map, (!))
 import qualified Data.Map as Map
 import Data.Map.Monoidal (MonoidalMap)
 import qualified Data.Map.Monoidal as MMap
@@ -30,7 +30,6 @@ import qualified Data.PQueue.Min as MinQueue
 import Data.Ratio
 import Data.Set (Set, member, union, (\\))
 import qualified Data.Set as Set
-import Data.Vector ((!))
 import qualified Data.Vector as V
 import Day3 (toInt)
 import Lib (MyParser, parseStdin)
@@ -42,9 +41,18 @@ import Text.Show.Functions
 
 newtype Board = Board {_getBoard :: [Int]}
 
+-- Equality for boards which assumes that if the head is equal, then the whole thing is equal.
+-- Won't work for boards of potentially different length.
+instance Eq Board where
+  (==) (Board (a : _)) (Board (b : _)) = a == b
+
+-- Assumes that the length is 10 :-P
+instance Show Board where
+  show (Board b) = show $ take 10 b
+
 makeLenses ''Board
 
-newtype Die = Die {_getDie :: [Int]}
+newtype Die = Die {_getDie :: [Int]} deriving (Eq, Show)
 
 makeLenses ''Die
 
@@ -54,6 +62,7 @@ data Player = Player
     _score :: Int,
     _continuingBoard :: Board
   }
+  deriving (Eq, Show)
 
 makeLenses ''Player
 
@@ -62,6 +71,7 @@ data Game = Game
     _players :: (Player, Player),
     _die :: Die
   }
+  deriving (Eq, Show)
 
 makeLenses ''Game
 
@@ -84,17 +94,52 @@ move n player =
 
 type GameState = S.StateT Game []
 
-quantumDieRoll :: Die -> [(Int, Int, Int)]
-quantumDieRoll (Die d) = (,,) <$> d <*> d <*> d
+-- tuple of probability and sum of 3 rolls
+quantumDieRoll :: Die -> [(Rational, Int)]
+quantumDieRoll (Die d) =
+  let rollTotals = (\a b c -> a + b + c) <$> d <*> d <*> d
+      n = toInteger $ length rollTotals
+      groupedRolls = NonEmpty.groupAllWith id rollTotals
+      groupProbability = (% n) . toInteger . NonEmpty.length
+      groupRoll = NonEmpty.head
+   in fmap (groupProbability &&& groupRoll) groupedRolls
+
+nextStepProbabilities :: Die -> Map Int [(Rational, Int)]
+nextStepProbabilities d =
+  let rolls = quantumDieRoll d
+   in Map.fromList $ do
+        i <- [1 .. 10]
+        let continuingBoard = drop (i - 1) . _getBoard $ board
+        let nextPositions = rolls & (mapped . _2) %~ head . (`drop` continuingBoard)
+        return (i, nextPositions)
+
+multiverse :: Int -> [(Rational, Int)]
+multiverse startPos =
+  let d = Die [1 .. 3]
+      nextStepProbs = nextStepProbabilities d
+      step :: (Rational, Int, Int) -> [(Rational, Int, Int)]
+      step (p, i, score) =
+        let steps = nextStepProbs ! i
+         in fmap (\(q, j) -> (p * q, j, score + j)) steps
+      -- I think there's something magical about the number 6,
+      -- due to vague maths.
+      blargh =
+        step (1, startPos, 0) >>= step >>= step >>= step >>= step >>= step
+          >>= step
+          >>= step
+   in fmap (\(a, b, c) -> (a, c)) blargh
+
+moveStream :: Int -> Int -> [Player]
+moveStream name startPos = undefined
 
 roll :: GameState [Int]
 roll = do
   game <- S.get
   let d = game ^. die
-  (roll1, roll2, roll3) <- S.lift $ quantumDieRoll d
+  (probability, rollTotal) <- S.lift $ quantumDieRoll d
   let game' = game & nRolls +~ 3
   S.put game'
-  return [roll1, roll2, roll3]
+  return [rollTotal]
 
 turn :: GameState ()
 turn = do
